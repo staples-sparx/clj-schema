075d7f8138b903568d7bb25743bf7bec986d2e2c
Alex Baranosky <alex@runa.com>
2012-09-02 22:16:23 -0700

    tests make a bit more sense now

--- test/clj_schema/fixtures_test.clj
+++ test/clj_schema/fixtures_test.clj
@@ -5,46 +5,46 @@
 
 (schema/def-validation-schema person-schema
   [[:name] String
-   [:height] clojure.lang.Keyword])
+   [:height] Number])
 
 (def fixture-1 (fixture person-schema {:name "Roberto"
-                                       :height :555}))
+                                       :height 555}))
 
 (def-fixture fixture-2 person-schema {:name "Roberto"
-                                      :height :555})
+                                      :height 555})
 
 (def-fixture-factory a-factory person-schema
   [& {:keys [name height]
       :or {name "Roberto"
-           height :555}}]
+           height 555}}]
   {:name name
    :height height})
 
 (def-fixture-factory b-factory person-schema
   ([]
     {:name "Roberto"
-     :height :555})
+     :height 555})
   ([name]
     {:name name
-     :height :555}))
+     :height 555}))
 
 (deftest test-fixture-goodness
   (testing "You can make fixtures in a variety of ways"
     (is (= {:name "Roberto"
-            :height :555}
+            :height 555}
           fixture-1
           fixture-2
           (a-factory)
           (b-factory)))
 
     (is (= {:name "Foo"
-            :height :111}
-          (a-factory :name "Foo" :height :111)))
+            :height 111}
+          (a-factory :name "Foo" :height 111)))
 
     (is (= {:name "Bar"
-            :height :555}
+            :height 555}
           (b-factory "Bar"))))
 
   (testing "return values that don't pass validation throw AssertionErrors"
-    (is (thrown? AssertionError (a-factory :name :555 :height "Bob")))
-    (is (thrown? AssertionError (b-factory :555)))))
+    (is (thrown? AssertionError (a-factory :name 555 :height "Bob")))
+    (is (thrown? AssertionError (b-factory 555)))))
