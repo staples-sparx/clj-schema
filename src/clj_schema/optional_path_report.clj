aaac8671ec1c6ccdee761b64cd249029bba8a32a
Alex Baranosky <alex@runa.com>
2012-09-24 03:25:15 -0500

    deleting optional path report spike

--- src/clj_schema/optional_path_report.clj
+++
@@ -1,7 +0,0 @@
-(ns clj-schema.optional-path-report
-  (:require [clj-schema.schema :as sch]))
-
-(defn optional-path-report [schema m]
-  {:all-optional-paths (sch/optional-path-set schema)
-   :present #{}
-   :absent #{}})
