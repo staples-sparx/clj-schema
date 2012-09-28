12135795606ff1fc4155bf17c6ccefcf293b4eb8
Alex Baranosky <alex@runa.com>
2012-09-28 01:28:58 -0700

    Improved ns docstring

--- src/clj_schema/schema.clj
+++ src/clj_schema/schema.clj
@@ -3,22 +3,42 @@
 
       Schemas are any number of paths through a nested map, paired with a validator.
 
-      Validators are either a single predicate or schema, or a seq of predicates
-      or schems (or a mix of predicates or schemas).
+      There are 5 types of validators:
+      * any predicate function
+      * any Class object, i.e. String, clojure.lang.Keyword, java.util.Date, etc
+      * any clj-schema schema
+      * [validator1 validator2] to indicate both validator1 AND validator2
+      * [:or validator1 validator2] to indicate both validator1 OR validator2
 
-      A path may also be marked as an `optional-path`. By default a value at a given
-      path is assumed to be a single value; to mark it sequential, wrap the validator
-      in the 'sequence-of' function. NOTE: 'nil' is a passing value for a 'sequence-of' validator.
+      Any validator may be wrapped in sequence-of to indicate the value should be
+      sequential, or wrapped in set-of to indicate the value is a set. By default,
+      schemas assume the value at a path to be a singular value.
 
-      Example Schema:
+      A path may be marked as an `optional-path`.  This means that is doesn't
+      have to be present, but if it is, it must match the given validator.
 
-      [[:a :b :c] pred
+      Wildcard paths are paths where one or more peices are defined as anything
+      matching a given validator.
+
+       Example Schema:
+
+      (defschema bar-schema
+       [[:a :b :c] pred
        [:x :y :z] [pred2 pred3 z-schema] ;; implicit 'and' - all three must pass
        [:p :q :r] [:or nil? r-schema]    ;; an 'or' statement - need just one to pass
        (optional-path [:z]) (sequence-of string?)
        [:a b :d] (loose-valdiation-schema [[:cat :name] String ;; can use Java Class objects directly
                                            [:cat :colors] (set-of String)])
       ... ]
+
+     Example schema w/ wildcard paths:
+
+      (defschema foo-schema
+        [[:a (wild String) (wild Number)] String])
+
+      => matches maps such as:
+         {:a {\"car\" {1.21 \"jigawatts\"}}}
+         {:a {\"banana\" {2 \"green\"
 
       `defschema` creates a strict schema, which expects only the paths it
       describes to be present on the given map.
