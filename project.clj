7ce999834fc5c971b5837b51380bdc54b8a5e0b9
Alex Baranosky <alex@runa.com>
2012-12-28 01:07:16 -0800

    add license to project file

--- project.clj
+++ project.clj
@@ -3,11 +3,18 @@
 (defproject org.clojars.runa/clj-schema "0.7.2"
   :description "Map schema definitions and validation library."
   :url "https://github.com/runa-dev/clj-schema"
+  :license {:name "MIT License"
+            :url "http://mit-license.org/"}
   :dependencies ~(cons '[org.clojure/clojure "1.2.0"]
                          common-deps)
   :dev-dependencies [[jonase/kibit "0.0.3"]
                      [jonase/eastwood "0.0.2"]
+                     [slamhound "1.3.0"]
                      [lein-multi "1.1.0"]]
+  :profiles {:dev {:dependencies [[jonase/kibit "0.0.3"]
+                                  [jonase/eastwood "0.0.2"]
+                                  [slamhound "1.3.0"]]}}
+  :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :multi-deps {"1.2.0" [[org.clojure/clojure "1.2.0"]]
                "1.2.1" [[org.clojure/clojure "1.2.1"]]
                "1.3.0" [[org.clojure/clojure "1.3.0"]]
